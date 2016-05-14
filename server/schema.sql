56e9712c8853c9cb7fd06e9a63198695122071ab
shams-ali <shams.ali0216@gmail.com>
2016-05-13 20:46:34 -0700

    created tables

--- server/schema.sql
+++ server/schema.sql
@@ -2,9 +2,28 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
+
+CREATE TABLE Rooms (
   /* Describe your table here.*/
+  id INTEGER NOT NULL PRIMARY KEY,
+  name CHAR(20) NOT NULL
 );
+CREATE TABLE Users (
+  /* Describe your table here.*/
+  id INTEGER NOT NULL PRIMARY KEY,
+  name CHAR(20) NOT NULL
+);
+CREATE TABLE Messages (
+  /* Describe your table here.*/
+  id INTEGER PRIMARY KEY,
+  message CHAR(140) NOT NULL,
+  user_id INTEGER NOT NULL,
+  room_id INTEGER NOT NULL,
+  FOREIGN KEY(user_id) REFERENCES Users(id),/*add fk user_id*/
+  FOREIGN KEY(room_id) REFERENCES Rooms(id)/*add fk room_id*/
+);
+
+
 
 /* Create other tables and define schemas for them here! */
 
